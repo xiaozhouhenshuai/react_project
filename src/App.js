30c1e33c82021e9c1840a3a8dcbb2015e5555760
li zhouzhou <1922629187@qq.com>
2020-04-27 21:30:55 +0800

    更新html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 // 该文件是最外层的壳子
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Button,Drawer } from 'antd'
 
 
 export default class App extends Component {
+  state = { visible: false };
+  showDrawer = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
   render() {
     return (
-      <div>
-        App.........
-        <Button type="primary">Button</Button>
+      <div className="site-drawer-render-in-current-wrapper">
+      Render in this
+      <div style={{ marginTop: 16 }}>
+        <Button type="primary" onClick={this.showDrawer}>
+          Open
+        </Button>
       </div>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        closable={false}
+        onClose={this.onClose}
+        visible={this.state.visible}
+        getContainer={false}
+        style={{ position: 'absolute' }}
+      >
+        <p>Some contents...</p>
+      </Drawer>
+    </div>
     )
   }
 }
