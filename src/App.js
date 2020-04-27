13ba925bd17aa110316332d1bc0b144501272c43
li zhouzhou <1922629187@qq.com>
2020-04-27 19:42:24 +0800

    更新

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// 该文件是最外层的壳子
+import React, { Component } from 'react'
+import { Button } from 'antd'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        App.........
+        <Button type="primary"></Button>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// 该文件是整个应用的入口文件,该文件需要渲染App
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App/>,document.getElementById('root'))
